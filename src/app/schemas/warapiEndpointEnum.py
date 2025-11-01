60edff995535696063705f6d0aac7c203fbf1de4
Goliaten <ognikat4@gmail.com>
2025-11-01 13:59:17 +0100

    typo in warapi endpoints

--- src/app/schemas/warapiEndpointEnum.py
+++ src/app/schemas/warapiEndpointEnum.py
@@ -4,7 +4,7 @@ from enum import Enum
 class warapiEndpoints(Enum):
     war_state = r"/worldconquest/war"
     # may be updated every 60 seconds
-    map_list = r"/wolrdconquest/maps"
+    map_list = r"/worldconquest/maps"
     # not all maps have available map data
     map_war_report = r"/worldconquest/warReport/{map_name}"
     # may be updated every 3 seconds
